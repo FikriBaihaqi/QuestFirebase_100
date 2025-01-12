cf4ad8f38c14a1a48104c4dd346834037b8a9cf3
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2025-01-13 06:17:15 +0700

    menambahkan isi dari fungsi updatemahasiswa

--- app/src/main/java/com/example/firebase/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase/repository/NetworkMahasiswaRepository.kt
@@ -39,7 +39,14 @@ class NetworkMahasiswaRepository(
     }
 
     override suspend fun updatemahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
     }
 
     override suspend fun deletemahasiswa(nim: String) {

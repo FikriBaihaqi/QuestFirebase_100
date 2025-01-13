af141a3ca9e5557cc38f00752858e385dc28dc04
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2025-01-13 07:38:33 +0700

    membuat data class variabel yang menyimpan data input form

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -66,3 +66,39 @@ class InsertViewModel (
         uiState = FormState.Idle
     }
 }
+
+sealed class FormState {
+    object Idle: FormState()
+    object Loading: FormState()
+    data class Success(val message: String): FormState()
+    data class Error(val message: String): FormState()
+}
+
+data class InsertUiState (
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenis_kelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
+//data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
